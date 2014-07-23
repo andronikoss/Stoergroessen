e5dd041fbceee8731ae1c64ed4dcc26bdbd37d81
andronikoss <and.stepanyan@gmail.com>
2014-07-23 18:42:38 +0200

    Update server.R

--- server.R
+++ server.R
@@ -7,14 +7,14 @@ shinyServer(function(input, output){
         sim <- input$action
         select <- input$Number.of.Samples
         dichte <- reactive({input$checkbox}) 
-        stoergroesse(select=select, dichte=dichte(), N=1000, sim=sim)
+        stoergroesse(select = select, dichte = dichte(), N = 1000, sim = sim)
 
         # Download
         output$downloadPlot <- downloadHandler(
-            filename = function() { paste("errterm", '.jpeg', sep='') },
+            filename = function() { paste("errterm", '.jpeg', sep = '') },
             content = function(file) {
                 jpeg(file,width = 1000, height = 600)
-                stoergroesse(select=select, dichte=dichte(), N=1000, sim = sim)
+                stoergroesse(select = select, dichte = dichte(), N = 1000, sim = sim)
                 dev.off()
             })
         
